dfb64bad18707e3a42761df4bf9e3fca3320298f
Alex Schickedanz <alex@ae.cs.uni-frankfurt.de>
2017-02-24 12:24:29 +0100

    removed int_types

--- tests/mng/test_async_schedule.cpp
+++ tests/mng/test_async_schedule.cpp
@@ -17,8 +17,6 @@
 #include <cstdlib>
 #include <random>
 
-using int_type = foxxll::int_type;
-
 // Test async schedule algorithm
 
 int main(int argc, char* argv[])
@@ -28,36 +26,36 @@ int main(int argc, char* argv[])
         STXXL_ERRMSG("Usage: " << argv[0] << " D L m seed");
         return -1;
     }
-    const int D = atoi(argv[1]);
-    const int L = atoi(argv[2]);
-    const int_type m = atoi(argv[3]);
-    uint32_t seed = atoi(argv[4]);
-    int_type* disks = new int_type[L];
-    int_type* prefetch_order = new int_type[L];
-    int* count = new int[D];
+    const size_t D = strtoul(argv[1], NULL, 0);
+    const size_t L = strtoul(argv[2], NULL, 0);
+    const size_t m = strtoul(argv[3], NULL, 0);
+    uint32_t seed  = strtoul(argv[4], NULL, 0);
+    size_t* disks  = new size_t[L];
+    size_t* prefetch_order = new size_t[L];
+    size_t* count  = new size_t[D];
 
-    for (int i = 0; i < D; i++)
+    for (size_t i = 0; i < D; i++)
         count[i] = 0;
 
     std::default_random_engine rnd(seed);
-    for (int i = 0; i < L; i++)
+    for (size_t i = 0; i < L; i++)
     {
         disks[i] = rnd() % D;
         count[disks[i]]++;
     }
 
-    for (int i = 0; i < D; i++)
+    for (size_t i = 0; i < D; i++)
         std::cout << "Disk " << i << " has " << count[i] << " blocks" << std::endl;
 
     foxxll::compute_prefetch_schedule(disks, disks + L, prefetch_order, m, D);
 
     STXXL_MSG("Prefetch order:");
-    for (int i = 0; i < L; ++i) {
+    for (size_t i = 0; i < L; ++i) {
         STXXL_MSG("request " << prefetch_order[i] << "  on disk " << disks[prefetch_order[i]]);
     }
     STXXL_MSG("Request order:");
-    for (int i = 0; i < L; ++i) {
-        int j;
+    for (size_t i = 0; i < L; ++i) {
+        size_t j;
         for (j = 0; prefetch_order[j] != i; ++j) ;
         STXXL_MSG("request " << i << "  on disk " << disks[i] << "  scheduled as " << j);
     }
