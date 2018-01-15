123216bdcde74563561952b7a24c513e170141f6
Manuel Penschuck <manuel@ae.cs.uni-frankfurt.de>
2018-01-15 10:17:16 +0100

    test_block_manager: bugfix (?). The code did not make much sense earlier ?!

--- tests/mng/test_block_manager.cpp
+++ tests/mng/test_block_manager.cpp
@@ -4,6 +4,7 @@
  *  Part of the STXXL. See http://stxxl.org
  *
  *  Copyright (C) 2002 Roman Dementiev <dementiev@mpi-sb.mpg.de>
+ *  Copyright (C) 2018 Manuel Penschuck <manuel@ae.cs.uni-frankfurt.de>
  *
  *  Distributed under the Boost Software License, Version 1.0.
  *  (See accompanying file LICENSE_1_0.txt or copy at
@@ -14,14 +15,15 @@
 //! This is an example of use of completion handlers, \c foxxll::block_manager, and
 //! \c foxxll::typed_block
 
+#include <iostream>
+#include <memory>
+#include <vector>
+
 #include <foxxll/io.hpp>
 #include <foxxll/mng.hpp>
 #include <foxxll/verbose.hpp>
 
-#include <iostream>
-#include <vector>
-
-#define BLOCK_SIZE (1024 * 512)
+constexpr size_t BLOCK_SIZE = 512 * 1024;
 
 struct MyType
 {
@@ -44,59 +46,50 @@ int main()
 {
     STXXL_MSG(sizeof(MyType) << " " << (BLOCK_SIZE % sizeof(MyType)));
     STXXL_MSG(sizeof(block_type) << " " << BLOCK_SIZE);
-    const unsigned nblocks = 2;
+
+    constexpr size_t nblocks = 2;
     foxxll::BIDArray<BLOCK_SIZE> bids(nblocks);
-    std::vector<int> disks(nblocks, 2);
-    foxxll::request_ptr* reqs = new foxxll::request_ptr[nblocks];
+
+    std::unique_ptr<foxxll::request_ptr[]> reqs(new foxxll::request_ptr[nblocks]);
     foxxll::block_manager* bm = foxxll::block_manager::get_instance();
     bm->new_blocks(foxxll::striping(), bids.begin(), bids.end());
 
-    block_type* block = new block_type[2];
+    std::unique_ptr<block_type[]> block(new block_type[nblocks]);
+
     STXXL_MSG(std::hex);
-    STXXL_MSG("Allocated block address    : " << (size_t)(block));
-    STXXL_MSG("Allocated block address + 1: " << (size_t)(block + 1));
+    STXXL_MSG("Allocated block address    : " << (size_t)(block.get()));
+    STXXL_MSG("Allocated block address + 1: " << (size_t)(block.get() + 1));
     STXXL_MSG(std::dec);
-    size_t i = 0;
-    for (i = 0; i < block_type::size; ++i)
-    {
-        block->elem[i].integer = i;
-        //memcpy (block->elem[i].chars, "STXXL", 4);
+
+    for(size_t i = 0; i < nblocks; i++) {
+        for (size_t j = 0; j < block_type::size; ++j) {
+            block[i].elem[j].integer = i + j;
+        }
     }
-    for (i = 0; i < nblocks; ++i)
-        reqs[i] = block->write(bids[i], my_handler());
+
+    for (size_t i = 0; i < nblocks; ++i)
+        reqs[i] = block[i].write(bids[i], my_handler());
 
     std::cout << "Waiting " << std::endl;
-    foxxll::wait_all(reqs, nblocks);
+    foxxll::wait_all(reqs.get(), nblocks);
 
-    for (i = 0; i < nblocks; ++i)
+    for(size_t i = 0; i < nblocks; i++) {
+        for (size_t j = 0; j < block_type::size; ++j) {
+            block[i].elem[j].integer = 0xdeadbeaf;
+        }
+    }
+
+    for (size_t i = 0; i < nblocks; ++i)
     {
-        reqs[i] = block->read(bids[i], my_handler());
+        reqs[i] = block[i].read(bids[i], my_handler());
         reqs[i]->wait();
         for (size_t j = 0; j < block_type::size; ++j)
         {
-            STXXL_CHECK2(j == block->elem[j].integer,
+            STXXL_CHECK2(i + j == block[i].elem[j].integer,
                          "Error in block " << std::hex << i << " pos: " << j
-                                           << " value read: " << block->elem[j].integer);
+                                           << " value read: " << block[i].elem[j].integer);
         }
     }
 
     bm->delete_blocks(bids.begin(), bids.end());
-
-    delete[] reqs;
-    delete[] block;
-
-#if 0
-    // variable-size blocks, not supported currently
-
-    BIDArray<0> vbids(nblocks);
-    for (i = 0; i < nblocks; i++)
-        vbids[i].size = 1024 + i;
-
-    bm->new_blocks(striping(), vbids.begin(), vbids.end());
-
-    for (i = 0; i < nblocks; i++)
-        STXXL_MSG("Allocated block: offset=" << vbids[i].offset << ", size=" << vbids[i].size);
-
-    bm->delete_blocks(vbids.begin(), vbids.end());
-#endif
 }
